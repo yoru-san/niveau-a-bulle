36502cd8d98bdf5d56b8381df1d7adc8d2573672
MissYoruwara <cecel31@free.fr>
2018-04-12 22:50:19 +0200

    fin mouvement bulle

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,20 @@
+var screenWidth, screenHeight, bubbleWidth, bubbleHeight;
+
 var app = {
     // Application Constructor
     initialize: function() {
-        if(window.cordova)
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    else 
-        this.onDeviceReady();
+        if(window.cordova) {
+            document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+            screenWidth  = $(window).width();
+            screenHeight = $(window).height();
+            bubbleWidth  = $("#bubble").outerWidth();
+            bubbleHeight = $("#bubble").outerHeight();
+
+            $("#vertical-line").css("left", screenWidth / 2 - 1);
+            $("#horizontal-line").css("top", screenHeight / 2 - 1);
+        } else 
+            this.onDeviceReady();
     },
 
     // deviceready Event Handler
@@ -17,7 +27,7 @@ var app = {
 var lastX;
 var lastY;
 //Définition de notre ratio de comparaison des valeurs
-var denoiserRatio = 10;
+var denoiserRatio = 2;
 function onSuccess (acceleration) {
 
     //degreZ = 180 / Math.PI * (Math.acos(acceleration.z/9.81));
@@ -27,37 +37,41 @@ function onSuccess (acceleration) {
 
     // Si l'ancienne valeur de x n'est pas défini, on l'affiche de suite dans le navigateur puis on la stocke en tant qu'ancienne valeur.
     if (!lastX) {
-        document.getElementById("x").innerHTML = degreX;
+        document.getElementById("x").innerHTML = "X : " + degreX;
         lastX = degreX;
+
+        var right = lastX / 180 * screenWidth - (bubbleWidth / 2);
+        $("#bubble").css("right", right + "px");
     } else {
         // Si l'ancienne valeur de x est défini, on compare les 2 valeurs de x pour garder la différence (on utilise la valeur absolue Math.abs pour éviter d'avoir une différence négative).
         //Si cette différence est supérieure à 1, on affiche la nouvelle valeur de x et on la stocke en tant qu'ancienne valeur.
         differenceX = Math.abs(degreX - lastX);
         if (differenceX > denoiserRatio) {
-            document.getElementById("x").innerHTML = degreX;
+            document.getElementById("x").innerHTML = "X : " + degreX;;
             lastX = degreX;
-            var marginX = lastX;
-            if (lastX >= 130)
-                marginX = 130;
-            $("#bubble").css("margin-top", marginX + "px");
+
+            var right = lastX / 180 * screenWidth - (bubbleWidth / 2);
+            $("#bubble").css("right", right + "px");
         }
     }
 
     // Si l'ancienne valeur de y n'est pas défini, on l'affiche de suite dans le navigateur puis on la stocke en tant qu'ancienne valeur.
     if (!lastY) {
-        document.getElementById("y").innerHTML = degreY;
-        lastY = degreX;
+        document.getElementById("y").innerHTML = "Y : " + degreY;
+        lastY = degreY;
+
+        var top = lastY / 180 * screenHeight - (bubbleHeight / 2);
+        $("#bubble").css("top", top + "px");
     } else {
         // Si l'ancienne valeur de y est défini, on compare les 2 valeurs de y pour garder la différence (on utilise la valeur absolue Math.abs pour éviter d'avoir une différence négative).
         //Si cette différence est supérieure à 1, on affiche la nouvelle valeur de y et on la stocke en tant qu'ancienne valeur.
         differenceY = Math.abs(degreY - lastY);  
         if (differenceY > denoiserRatio) {
-            document.getElementById("y").innerHTML = degreY;
+            document.getElementById("y").innerHTML = "Y : " + degreY;
             lastY = degreY;
-            var marginY = lastY;
-            if (lastY >= 130)
-                marginY = 130;
-            $("#bubble").css("margin-left", marginY + "px");
+
+            var top = lastY / 180 * screenHeight - (bubbleHeight / 2);
+            $("#bubble").css("top", top + "px");
         }    
     }
 }
